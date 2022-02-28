1f3ccd3034a8d314bbaf1146d1cbdf5b1d0bb372
kuon57 <louie999111@hotmail.com>
2022-02-28 20:20:09 +0800

    init

---
+++ EnumFont/EnumFont.h
@@ -0,0 +1,24 @@
+#ifndef _ENUMFONT_H_
+#define _ENUMFONT_H_
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <Windows.h>
+
+class EnumFont{
+public:
+	EnumFont();
+
+	std::vector<std::string> GetFonts();
+	void StartEnumFonts();
+
+	int CALLBACK EnumTheFonts(LOGFONT *lpLF, TEXTMETRIC *lpTM, DWORD dwTyp, LPARAM lpData);
+
+private:
+	std::vector<std::string> m_fontList;
+};
+
+
+
+#endif
